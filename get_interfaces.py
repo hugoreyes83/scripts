198c5013437fd2d31a77c211d3f674346c394d66
hugoreyes83 <reyes83@gmail.com>
2018-01-29 19:17:30 -0800

    changes to get_interfaces.py

--- get_interfaces.py
+++ get_interfaces.py
@@ -15,8 +15,8 @@ from jnpr.junos.exception import CommitError
 from jnpr.junos.exception import ConfigLoadError
 
 parser = argparse.ArgumentParser(description='script for gathering interfaces data from a router')
-parser.add_argument('--device')
-parser.add_argument('--username')
+parser.add_argument('--device',required=True)
+parser.add_argument('--username',required=True)
 args = parser.parse_args()
 
 def get_password():
@@ -24,33 +24,49 @@ def get_password():
     return password
 
 def main():
-    password = get_password()
-    mx_router = get_network_driver('junos')
-    router = mx_router(hostname=args.device,username=args.username,password=password)
-    router.open()
-    if not router.is_alive():
-        print 'router is not alive'
-    else:
-        header = ['Interface', 'Description', 'Last Flapped', 'Status', 'MAC Address', 'Speed']
-        table = PrettyTable(header)
-        interfaces = router.get_interfaces()
-        lldp_neighbors = router.get_lldp_neighbors()
-        for i in interfaces:
-            match_ge_interfaces = re.match(r'(^ge-*)', i)
-            if match_ge_interfaces:
-                port = i
-                desc =  interfaces[i]['description']
-                flap = str(datetime.timedelta(seconds=interfaces[i]['last_flapped']))
-                #state =  interfaces[i]['is_up']
-                if interfaces[i]['is_up'] == True:
-                    state = 'UP'
+    try:
+        password = get_password()
+        mx_router = get_network_driver('junos')
+        router = mx_router(hostname=args.device,username=args.username,password=password)
+        router.open()
+        if not router.is_alive():
+            print 'router is not alive'
+        else:
+            header = ['Interface', 'Description', 'Last Flapped', 'Status', 'MAC Address', 'Speed', 'LLDP Neighbor', 'Remote Interface']
+            table = PrettyTable(header)
+            interfaces = router.get_interfaces()
+            lldp_neighbors = router.get_lldp_neighbors()
+            list_of_lldp_interfaces = []
+            for i in lldp_neighbors:
+                list_of_lldp_interfaces.append(i)
+            for i in interfaces:
+                match_ge_interfaces = re.match(r'(^ge-*)', i)
+                if match_ge_interfaces:
+                    port = i
+                    desc =  interfaces[i]['description']
+                    flap = str(datetime.timedelta(seconds=interfaces[i]['last_flapped']))
+                    if interfaces[i]['is_up'] == True:
+                        state = 'UP'
+                    else:
+                        state = 'DOWN' 
+                    mac =  interfaces[i]['mac_address']
+                    speed = interfaces[i]['speed']
+                    if i in list_of_lldp_interfaces:
+                        header = ['Interface', 'Description', 'Last Flapped', 'Status', 'MAC Address', 'Speed', 'LLDP Neighbor', 'Remote Interface']
+                        table = PrettyTable(header)
+                        lldp_neighbor =  lldp_neighbors[i][0]['hostname']
+                        lldp_port = lldp_neighbors[i][0]['port']
+                        table.add_row([port,desc,flap,state,mac,speed,lldp_neighbor,lldp_port])
+                    else:
+                        table.add_row([port,desc,flap,state,mac,speed,'No lldp neighbor','N/A'])
                 else:
-                    state = 'DOWN' 
-                mac =  interfaces[i]['mac_address']
-                speed = interfaces[i]['speed']
-                table.add_row([port,desc,flap,state,mac,speed])
-            else:
-                continue
-        print table
+                    continue
+            print table
+    except ConnectAuthError as e:
+        print e
+    except ConnectRefusedError as e:
+        print e
+
 if __name__ == '__main__':
     main()
+
