1e213902304f7e62b62f30c5439333a9ce472ec2
Hugo Reyes <reyes83@gmail.com>
2018-07-01 22:11:04 -0700

    arp scanner

---
+++ arp_scanner.py
@@ -0,0 +1,24 @@
+from scapy.all import srp,Ether,ARP,conf 
+import sys
+from datetime import datetime
+
+
+try:
+    interface = raw_input('[*] Please enter interface ')
+    ips = raw_input('[*] Please enter IPs ')
+        
+    print '\n[*]Scanning'
+    start_time = datetime.now()
+    conf.verb = 0
+    ans,unans = srp(Ether(dst = "ff:ff:ff:ff:ff:ff")/ARP(pdst = ips), timeout = 10, iface = interface, inter = 0.1)
+    print 'MAC  -  IP\n'
+    for snd,rcv in ans:
+        print rcv.sprintf(r'%Ether.src%  - %ARP.psrc%')
+    stop_time = datetime.now()
+    total_time = stop_time - start_time
+    print '\n[*]Scan Complete'
+    print '[*]Scan Duration: {}'.format(total_time)
+
+except KeyboardInterrupt:
+        print 'Quitting'
+        sys.exit(1)
