08dbac0ee6dce6994c64c72dd5d8daef94b98773
Hugo Reyes <reyes83@gmail.com>
2018-08-13 05:07:54 +0300

    added v2 and v3 to is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,8 +1,32 @@
-def is_prime(n):
+import math
+
+def is_prime_v1(n):
     """Return 'True' is n is prime, return 'False' otherwise"""
     if n == 1:
         return False
     for d in range(2,n):
+        if n % d == 0:
+            return False
+    return True
+
+def is_prime_v2(n):
+    """Return 'True' is n is prime, return 'False' otherwise"""
+    if n == 1:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(2,max_divisor + 1):
+        if n % d == 0:
+            return False
+    return True
+
+def is_prime_v3(n):
+    """Return 'True' is n is prime, return 'False' otherwise"""
+    if n == 1:
+        return False
+    if n > 2 or n % 2 == 0:
+        return False
+    max_divisor = math.floor(math.sqrt(n))
+    for d in range(3, max_divisor + 1, 2):
         if n % d == 0:
             return False
     return True
