e29fe74acef5974560cb08f160af949b1b9e769a
Hugo Reyes <reyes83@gmail.com>
2018-08-30 22:56:28 -0700

    fixing is_prime.py script

--- is_prime.py
+++ is_prime.py
@@ -23,7 +23,7 @@ def is_prime_v3(n):
     """Return 'True' is n is prime, return 'False' otherwise"""
     if n == 1:
         return False
-    if n > 2 or n % 2 == 0:
+    if n < 2 or n % 2 == 0:
         return False
     max_divisor = math.floor(math.sqrt(n))
     for d in range(3, max_divisor + 1, 2):
