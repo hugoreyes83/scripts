39825664d4623f9eb1090fdd6765e9031cf22d58
Hugo Reyes <reyes83@gmail.com>
2018-12-17 22:56:28 -0800

    fixes to decorate.py

--- decorate.py
+++ decorate.py
@@ -10,15 +10,15 @@ for i in decoratestring('this is not a test'):print(i)
 
 def decoratetext(t):
     list1 = []
-    top = ' {}'.format('-'*len(max([ i for i in t.splitlines()], key = lambda x: len(x))))
-    pattern = '| %%-%ds |'% len(max([ i for i in t.splitlines()], key = lambda x: len(x)))
-    middle= [pattern %i for i in t.splitlines()]
-    bottom = ' {}'.format('-'*len(max([ i for i in t.splitlines()], key = lambda x: len(x))))
+    top = ' {}'.format('-'*len(max([ i for i in t], key = lambda x: len(x))))
+    pattern = '| %%-%ds |'% len(max([ i for i in t], key = lambda x: len(x)))
+    middle= [pattern %i for i in t]
+    bottom = ' {}'.format('-'*len(max([ i for i in t], key = lambda x: len(x))))
     list1.append(top)
     list1.extend(middle)
     list1.append(bottom)
     return list1
 
-multilinetext ='first line\nsecond line\nthird line\nfourth line,this is of course the fifth line'
+multilinetext ='first line\nsecond line\nthird line\nfourth line\nthis is of course the fifth line'
 
 for i in decoratetext(multilinetext): print(i)
