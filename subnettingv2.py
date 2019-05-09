40957715ec1f532fe1a85325be48a95160eb5b64
Reyes <reyeshug@c4b301cf6da3.ant.amazon.com>
2019-05-09 16:12:14 -0700

    more changes to subnettingv2.py

--- subnettingv2.py
+++ subnettingv2.py
@@ -1,5 +1,6 @@
 from netaddr import IPNetwork
 from iptools import IpRange
+from ipaddress import ip_network
 from iptools.ipv4 import validate_cidr
 from prettytable import PrettyTable
 import argparse
@@ -22,31 +23,49 @@ def validate_prefix(ip):
     # validate both prefix and prefix length
     return validate_cidr(ip)
 
-def user_input():
+def user_input(p):
     # get user input
-    user_input = input('Enter prefix length ')
+    user_input = input('Enter prefix length: ')
     try:
         int(user_input)
-        if int(user_input) <= 32 and int(user_input) >= 1:
+        if int(user_input) <= 32 and int(user_input) >= 1 and int(user_input) > int(p):
             logging.info('{} is a valid prefix length'.format(user_input))
             return user_input
         else:
             logging.info('{} is not a valid prefix length'.format(user_input))
-    except:
-        logging.info('{} is not a valid prefix length'.format(user_input))
+            return False
+    except ValueError as e:
+        logging.info(e)
 
 def netmask(n):
     return '{}{}'.format('0'*n,'1'*(32-n))
 
+def generate_subnets(s,p):
+    return list(ip_network(s).subnets(new_prefix=p))
+
+def generate_table(d):
+    table = PrettyTable()
+    table.field_names = ["Prefixes"]
+    for i in d:
+        table.add_row([i])
+    return table
+
+
 def main():
+    #grab arguments
     get_args = parse_args()
     check_prefix = validate_cidr(get_args.prefix)
+    #check if prefix is valid, if it is not then do not proceed
     assert(check_prefix), 'Prefix is NOT a valid ipv4 prefix, Exiting...'
-    validate_prefix_length = user_input()
-    logging.info('Generating number of /{} that could fit in {}'.format(validate_prefix_length,get_args.prefix))
-    number_of_subnets = len(IpRange(get_args.prefix))//int(netmask(int(validate_prefix_length)),2)
-    logging.info('There are up to {} in {}'.format(number_of_subnets,get_args.prefix))
-
+    #ask user for prefix length
+    prefix_length = user_input(get_args.prefix.split('/')[1])
+    logging.info('Generating number of /{} that could fit in {}'.format(prefix_length,get_args.prefix))
+    assert(prefix_length), 'Provided prefix length cannot be larger than {}'.format(get_args.prefix.split('/')[1])
+    #generate subnets
+    generatesubnets = generate_subnets(get_args.prefix,int(prefix_length))
+    #generate table
+    generatetable = generate_table(generatesubnets)
+    print(generatetable)
 
 
 if __name__ == '__main__':
