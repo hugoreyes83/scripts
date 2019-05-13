d840a737d40254ffa01e08fdee81331bfdc30f5a
Reyes <reyeshug@c4b301cf6da3.ant.amazon.com>
2019-05-13 14:17:07 -0700

    code_challenge_2.py

---
+++ code_challenge_2.py
@@ -0,0 +1,13 @@
+def find_limit(n):
+    left,right = None,None
+    sortedlist = sorted(n)
+    for i in range(len(n)):
+        if n[i] != sortedlist[i] and left is None:
+            left = i
+        elif n[i] != sortedlist[i]:
+            right = i
+    return left,right
+
+list1 = [2,3,6,1,5]
+
+print(find_limit([list1]))
